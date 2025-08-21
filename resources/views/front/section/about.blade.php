18866a838430c5ee87bbb59306cdb92c647363e0
Asilbek Shoyzaqov <87477943+asilbek-shoyzaqov@users.noreply.github.com>
2025-08-21 10:18:14 +0500

    Thursday change commit

--- resources/views/front/section/about.blade.php
+++ resources/views/front/section/about.blade.php
@@ -1,42 +1,27 @@
 <!-- About Section -->
-<section id="about" class="about section">
+<div class="container">
+    <div class="row justify-content-center">
 
-    <!-- Section Title -->
-    <div class="container section-title" data-aos="fade-up">
-        <h2>{{ $item->menu->{'name_'.app()->getLocale()} }}</h2>
-        <p>{{ $item->{'name_'.app()->getLocale()} }}</p>
-    </div><!-- End Section Title -->
+        <div class="col-lg-10">
+            <!-- Blog Details Section -->
+            <section id="blog-details" class="blog-details section">
+                <div class="container">
 
-    <div class="container" data-aos="fade-up">
-        <div class="row gx-0 mb-5">
+                    <article class="article">
 
-            <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
-                <div class="content">
-                    <h2>{{ optional($item->services->first())->{'name_'.app()->getLocale()} }}</h2>
-                    <p>{{ optional($item->services->first())->{'body_'.app()->getLocale()} }}</p>
+                        <h2 class="title">{{ $item->{'name_'.app()->getLocale()} }}</h2>
+
+                        <div class="content">
+                            <p>{!! $item->{'body_'.app()->getLocale()} !!}</p>
+                        </div><!-- End post content -->
+
+                    </article>
+
                 </div>
-            </div>
-
-            <div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
-                <img src="{{ asset('storage/' . optional($item->services->first())->image) }}" class="img-fluid" alt="">
-            </div>
-
+            </section><!-- /Blog Details Section -->
         </div>
 
-        <div class="row gx-0">
-
-            <div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
-                <img src="{{ asset('storage/' . optional($item->services->skip(1)->first())->image) }}" class="img-fluid" alt="">
-            </div>
-
-            <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
-                <div class="content">
-                    <h2>{{ optional($item->services->skip(1)->first())->{'name_'.app()->getLocale()} }}</h2>
-                    <p>{{ optional($item->services->skip(1)->first())->{'body_'.app()->getLocale()} }}</p>
-                </div>
-            </div>
-
-        </div>
     </div>
+</div>
 
-</section><!-- /About Section -->
+
